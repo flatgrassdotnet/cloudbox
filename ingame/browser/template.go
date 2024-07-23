c3f234711c7a129f188b72ade889a52ea78fbf9d
Pancakes <patapancakes@pagefault.games>
2024-07-23 10:37:35 -0400

    Browser styling adjustments to work better on low res screens

--- ingame/browser/template.go
+++ ingame/browser/template.go
@@ -4,14 +4,14 @@ const tmpl = `<html>
 	<head>
 		<title>reboxed</title>
 		<style>
-			body {margin: 0px; font-family: Helvetica; background-color: #36393D; color: #EEE; padding-bottom: 50px;}
+			body {margin: 0px; font-family: Helvetica; background-color: #36393D; color: #EEE;}
 			a {color: #FFF; text-decoration: none;}
 			a:hover {color: #0AF;}
 			.nav {padding: 8px; background-color: #4096EE; height: 20px; border-bottom: 1px solid #90C6FE; box-shadow: 0px 16px 16px rgba(0, 0, 0, 0.1);}
-			.nav a {margin: 20px; font-size: 20px; font-weight: bolder;}
-			.logo h1 {padding-right: 20px; margin: 0px; font-size: 20px; font-style: italic; float: right; color: #FFF;}
-			.items {padding: 16px 8px;}
-			.pagenav {padding: 0px 20px; float: right;}
+			.nav a {margin: 10px; font-size: 20px; font-weight: bolder;}
+			.logo h1 {margin: 0px; font-size: 20px; font-style: italic; float: right; color: #FFF;}
+			.items {padding: 16px 8px; height:80%; overflow: auto;}
+			.pagenav {padding: 16px 8px; float: right;}
 			.pagenav a {margin: 8px; font-weight: bolder;}
 			.item {margin-left: 2px; margin-right: 2px; display: inline-block; font-size: 11px; font-weight: bolder; width: 128px; height: 125px; text-align: center; text-shadow: 1px 1px 1px #000; text-overflow: ellipsis; overflow: hidden; white-space: nowrap; letter-spacing: -0.1px;}
 			.item img {width: 128px; height: 100px;}
