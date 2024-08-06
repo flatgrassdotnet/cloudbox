aae2681c69b49aa28c2b0a457857b4010e100c0a
Pancakes <patapancakes@pagefault.games>
2024-08-06 03:43:02 -0400

    Add copyright notice across files

--- utils/discord.go
+++ utils/discord.go
@@ -1,3 +1,21 @@
+/*
+	reboxed - the toybox server emulator
+	Copyright (C) 2024  patapancakes <patapancakes@pagefault.games>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package utils
 
 import (
@@ -28,9 +46,9 @@ func SendDiscordMessage(url string, steamid int64, content string) error {
 	}
 
 	body, err := json.Marshal(DiscordWebhookRequest{
-		Username: u.PersonaName,
+		Username:  u.PersonaName,
 		AvatarURL: u.Avatar,
-		Content: content,
+		Content:   content,
 	})
 	if err != nil {
 		return err
